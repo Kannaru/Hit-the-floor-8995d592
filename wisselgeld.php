beaa140f8d0e4a33b7cd97beab31591d33e3e09e
Mark Verrips <2083591@talnet.nl>
2020-02-13 14:15:28 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,14 @@
+<?php 
+
+$input = $argv[1];
+$geld = [10, 5 ,2 ,1];
+
+foreach ($geld as $geldvalue ){
+    if (floor($input / $geldvalue) > 0) {
+        $amount = floor($input / $geldvalue);
+        echo $amount . " x " . $geldvalue . " euro" . PHP_EOL;
+        $input = $input - ($amount * $geldvalue);  
+    }
+}
+
+?>
